2104b6f829764c90216312e152d29256ddaafdef
Dennis-050 <gm0515032022@unab.edu.sv>
2023-04-18 20:31:50 -0600

    parcial-2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// parcial-2//
+
+int datol =16;
+string dato2 = "25g";
+
+try
+{
+    int resultado = datol + Convert.ToInt32(dato2);
+    Console.WriteLine(resultado);
+}
+catch (Exception)
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
